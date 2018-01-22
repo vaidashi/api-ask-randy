2a85f9bedb326533523f2341591992f520d12864
Ashish Vaid <vaid50@yahoo.com>
2018-01-22 11:10:53 -0700

    enabled cors in config folder

--- app/controllers/api/v1/chat_controller.rb
+++ app/controllers/api/v1/chat_controller.rb
@@ -3,9 +3,9 @@ class Api::V1::Chat::ChatController < ApplicationController
 
 
   def create(words)
-    ENV["app_id"]
-    ENV["user_key"]
-
+    @conn = Faraday.get("https://aiaas.pandorabots.com/talk/#{ENV["app_id"]}/randy?user_key=#{ENV["user_key"]}&input=${words}")
+    # @conn = Faraday.get("https://aiaas.pandorabots.com/bot/#{ENV[app_id]}?user_key=#{ENV["user_key"]}")
+    # binding.pry
   end
 
 
